4d11eddb946f99f6e8a572910be23b49697750d8
ujjwal1966 <ujjwalshukla1711@gmail.com>
2020-06-14 01:02:13 +0530

    my sql integration

--- middleware/routes.js
+++ middleware/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const handlers = require('../controller/controller');
 const router = express.Router();
 
-//to add a new profile
+//to add a new profile or update it
 router.post('/profile', handlers.updateProfile);
 
 //to get profile
 router.get('/profile', handlers.getProfile);
+
+// to  get feed
 router.get('/feed', handlers.getFeed);
 
 module.exports = router;
--- model/model.js
+++ model/model.js
@@ -1,57 +1,73 @@
-let AWS = require("aws-sdk");
-let awsConfig = {
-	"region" : "ap-south-1",
-	"endpoint" : "http://dynamodb.ap-south-1.amazonaws.com",
-	"accessKeyId" : "AKIAIS3DDEBR27HJUGCA",
-	"secretAccessKey" : "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"
-};
+const mysql = require('mysql');
 
-AWS.config.update(awsConfig);
+const db = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '',
+	database : 'jobFinder'
+});
 
-let dynamoDb = new AWS.DynamoDB.DocumentClient();
+db.connect((err) => {
+	if(err){
+		throw err;
+	}
+	console.log('mysql connected....');
+});
 
 class Model{
 	updateProfile(profile){
 		return new Promise((resolve, reject) => {
-			const params = {
-				TableName: 'Profile',
-				Item: profile
-			};
-			dynamoDb.put(params, (err, data) => {
-				if (err) {
+			let query = `INSERT INTO PROFILE SET ?`; //ON DUPLICATE KEY UPDATE ${JSON.stringify(profile)}`;
+			db.query(query, profile, (err, result) => {
+				if(err) {
+					console.log(err);
 					return reject(err);
 				}
-				return resolve(data);
+				return resolve(result);
 			});
 		});
 	}
 
 	getProfile(email) {
 		return new Promise((resolve, reject) => {
-			const params = {
-				TableName: 'Profile',
-				Key: {
-					email: email
-				}
-			};
-			dynamoDb.get(params, (err, data) => {
-				if (err) {
+			let query = ` SELECT * FROM PROFILE WHERE email = "${email}"`;
+			db.query(query, (err, result) => {
+				if(err) {
 					return reject(err);
 				}
-				console.log(data);
-				return resolve(data);
+				if(result.length === 0) {
+					return resolve([]);
+				}
+				result[0].otherLinks = JSON.parse(result[0].otherLinks);
+				return resolve(result);
 			});
 		});
 	}
 
-	feed(params) {
+	feed(offset, limit, filterBy, filterValue) {
 		return new Promise((resolve, reject) => {
-			dynamoDb.scan(params, (err, data) => {
-				if (err) {
+			let query;
+			if(filterBy) {
+				query = `SELECT * FROM  PROFILE WHERE ${filterBy} = "${filterValue}" AND isVerified = 0 ORDER BY updatedOn DESC LIMIT ${limit}`;
+			} else {
+				query = `SELECT * FROM  PROFILE WHERE isVerified = 0 ORDER BY updatedOn DESC LIMIT ${limit}`;
+			}
+			if(offset){
+				query +=  ` OFFSET ${offset}`;
+			}
+			db.query(query, (err, result) => {
+				if(err) {
 					console.log(err);
 					return reject(err);
 				}
-				return resolve(data);
+				result.forEach(element => {
+					element.otherLinks = JSON.parse(element.otherLinks);
+				});
+				let finalResp = { data : result};
+				if(result.length === limit) {
+					finalResp.offset = offset ? offset + limit : limit;
+				}
+				return resolve(finalResp);
 			});
 		});
 	}
