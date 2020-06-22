c4d48858c61533f3b453ca2895cfec87a3f8a342
ujjwal1966 <ujjwalshukla1711@gmail.com>
2020-06-23 00:12:53 +0530

    connection pool added for mysql

--- model/model.js
+++ model/model.js
@@ -1,18 +1,12 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+	connectionLimit : 10,
 	host : 'localhost',
 	user : 'root',
 	password : '',
 	database : 'jobFinder'
-});
-
-db.connect((err) => {
-	if(err){
-		throw err;
-	}
-	console.log('mysql connected....');
-});
+})
 
 class Model{
 	updateProfile(profile, updateIsVerified){
